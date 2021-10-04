27e2c77d1d62e181460e0211d33027fbab643d80
Ethan Fie <ethan.fie@ivanti.com>
2021-10-04 08:51:33 -0500

    remove eslintcache inline conditional styling

--- ReactTheCompleteGuide/styling/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ ReactTheCompleteGuide/styling/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -8,6 +8,9 @@ const CourseInput = (props) => {
   const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
@@ -24,7 +27,14 @@ const CourseInput = (props) => {
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label style={{ color: isValid ? "black" : "red" }}>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input
+          style={{
+            borderColor: isValid ? "black" : "red",
+            background: isValid ? "white" : "salmon",
+          }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
